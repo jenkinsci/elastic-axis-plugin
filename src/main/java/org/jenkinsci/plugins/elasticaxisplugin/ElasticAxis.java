a7ef1b88e50f0df360c0fd6d0a47e734533260f4
takeuchi <g.takeuchi@gmail.com>
2013-05-10 16:27:04 -0300

    Added label validation, auto complete and the possibility to add multiple labels

--- src/main/java/org/jenkinsci/plugins/elasticaxisplugin/ElasticAxis.java
+++ src/main/java/org/jenkinsci/plugins/elasticaxisplugin/ElasticAxis.java
@@ -1,22 +1,32 @@
 package org.jenkinsci.plugins.elasticaxisplugin;
 
 import hudson.Extension;
+import hudson.Util;
 import hudson.matrix.Axis;
 import hudson.matrix.AxisDescriptor;
 import hudson.matrix.LabelAxis;
 import hudson.matrix.MatrixBuild;
+import hudson.model.AutoCompletionCandidates;
+import hudson.model.Messages;
 import hudson.model.Computer;
+import hudson.model.Label;
 import hudson.model.Node;
+import hudson.model.labels.LabelAtom;
+import hudson.util.FormValidation;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+
+import antlr.ANTLRException;
 
 public class ElasticAxis extends LabelAxis {
 
@@ -54,9 +64,12 @@ public class ElasticAxis extends LabelAxis {
     
 	private static List<String> computeNodesInLabel(String labelWithNodes, boolean restrictToOnlineNodes) {
 		List<String> computedNodes=new ArrayList<String>();
-		for(Node node : Jenkins.getInstance().getLabel(labelWithNodes).getNodes()) {
-		  if (shouldAddNode(restrictToOnlineNodes, node.toComputer())) 
-		      computedNodes.add(node.getDisplayName());
+		String[] labels = labelWithNodes.split(",");
+		for (String aLabel : labels) {
+			for(Node node : Jenkins.getInstance().getLabel(aLabel.trim()).getNodes()) {
+				if (shouldAddNode(restrictToOnlineNodes, node.toComputer())) 
+					computedNodes.add(node.getDisplayName());
+			}
 		}
 		
         return Collections.unmodifiableList(computedNodes);
@@ -85,6 +98,48 @@ public class ElasticAxis extends LabelAxis {
                     formData.getString("labelString"),
                     formData.getBoolean("ignoreOffline")
             );
+        }
+        
+        public FormValidation doCheckLabelString(@QueryParameter String value) {
+        	String[] labels = value.split(" ");
+        	for (String oneLabel : labels) {
+        		FormValidation validation = checkOneLabel(oneLabel.trim());
+				if (!validation.equals(FormValidation.ok()))
+					return validation;
+			}
+            return FormValidation.ok();
+        }
+
+		private FormValidation checkOneLabel(String oneLabel) {
+			if (Util.fixEmpty(oneLabel)==null)
+				return FormValidation.ok(); 
+			
+			Label l = Jenkins.getInstance().getLabel(oneLabel);
+			if (l.isEmpty()) {
+				for (LabelAtom a : l.listAtoms()) {
+					if (a.isEmpty()) {
+						LabelAtom nearest = LabelAtom.findNearest(a.getName());
+						return FormValidation.warning(Messages.AbstractProject_AssignedLabelString_NoMatch_DidYouMean(a.getName(),nearest.getDisplayName()));
+					}
+				}
+				return FormValidation.warning(Messages.AbstractProject_AssignedLabelString_NoMatch());
+			}
+			return FormValidation.ok();
+		}
+        
+        public AutoCompletionCandidates doAutoCompleteLabelString(@QueryParameter String value) {
+            AutoCompletionCandidates c = new AutoCompletionCandidates();
+            Set<Label> labels = Jenkins.getInstance().getLabels();
+            List<String> queries = new AutoCompleteSeeder(value).getSeeds();
+
+            for (String term : queries) {
+                for (Label l : labels) {
+                    if (l.getName().startsWith(term)) {
+                        c.add(l.getName());
+                    }
+                }
+            }
+            return c;
         }
     }
 
