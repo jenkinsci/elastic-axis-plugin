05febcfe673e82c7e3b855051a16b89ad7b535d7
Mark Waite <mark.earl.waite@gmail.com>
2021-03-06 18:17:11 -0700

    Add rudimentary test class

---
+++ src/test/java/org/jenkinsci/plugins/elasticaxisplugin/ElasticAxisTest.java
@@ -0,0 +1,62 @@
+package org.jenkinsci.plugins.elasticaxisplugin;
+
+import java.util.Random;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Mark Waite
+ */
+public class ElasticAxisTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    private static final Random RANDOM = new Random();
+    private static final boolean IGNORE_OFFLINE = RANDOM.nextBoolean();
+    private static final boolean DO_NOT_EXPAND_LABELS = RANDOM.nextBoolean();
+    private static final String NAME_SUFFIX = "-" + (100 + RANDOM.nextInt(899)); // 3 digit random number
+    private static final String AXIS_NAME = "axis-name" + NAME_SUFFIX;
+    private static final String LABEL_STRING = "label-string" + NAME_SUFFIX;
+
+    private ElasticAxis elasticAxis;
+
+    public ElasticAxisTest() {
+    }
+
+    @Before
+    public void setUp() {
+        elasticAxis = new ElasticAxis(AXIS_NAME, LABEL_STRING, IGNORE_OFFLINE, DO_NOT_EXPAND_LABELS);
+    }
+
+    @Test
+    public void equalsContract() {
+        EqualsVerifier.forClass(ElasticAxis.class)
+                .usingGetClass()
+                .withIgnoredFields("values")
+                .verify();
+    }
+
+    @Test
+    public void testGetLabelString() {
+        assertThat(elasticAxis.getLabelString(), is(LABEL_STRING));
+    }
+
+    @Test
+    public void testGetIgnoreOffline() {
+        assertThat(elasticAxis.getIgnoreOffline(), is(IGNORE_OFFLINE));
+    }
+
+    @Test
+    public void testGetDontExpandLabels() {
+        assertThat(elasticAxis.getDontExpandLabels(), is(DO_NOT_EXPAND_LABELS));
+    }
+}
